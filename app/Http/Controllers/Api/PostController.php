ca95f69127736ea6c567547fd6b411c4b25e8b52
ibrahimmuhammad13720 <ibrahimmuhammad13720@gmail.com>
2022-07-06 00:54:58 +0200

    deploy app

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\StorePostsRequest;
+use App\Http\Controllers\Controller;
+use App\Models\Posts;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $posts = Posts::all();
+        return response()->json([
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePostsRequest $request)
+    {
+        //
+        $posts = Posts::create($request->all());
+
+    return response()->json([
+        'message' => "post saved successfully!",
+        'posts' => $posts
+    ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Posts  $posts
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Posts $posts)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Posts  $posts
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Posts $posts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Posts  $posts
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Posts $posts)
+    {
+        //
+        $posts->update($request->all());
+
+    return response()->json([
+        'message' => "post updated successfully!",
+        'posts' => $posts
+    ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Posts  $posts
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Posts $posts)
+    {
+        //
+        $posts->delete();
+
+    return response()->json([
+        'message' => "post deleted successfully!",
+    ], 200);
+    }
+}
