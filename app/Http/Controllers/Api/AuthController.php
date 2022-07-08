4d2f8594e31437ad0d9b743c3c0ce882f8830ac3
ibrahimmuhammad13720 <ibrahimmuhammad13720@gmail.com>
2022-07-08 15:48:52 +0200

    users class

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-use User;
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
